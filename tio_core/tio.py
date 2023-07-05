40b6b212a0fee4740aac64cbce4b181a06b07673
xj <xj@bytedance.com>
2023-07-05 15:15:49 +0800

    feat: 添加app-simple对应的client

--- tio_core/tio.py
+++ tio_core/tio.py
@@ -50,3 +50,52 @@ class ChatTiO():
         self.dialog += [(text, text_response)]
         others = {"bboxes": bboxes_2 + bboxes_1}
         return text_response, others
+
+
+def get_model(url="http://127.0.0.1:7860/"):
+    import tempfile
+    import gradio_client
+    client = gradio_client.Client(url)
+
+    def chat(image, text):
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as image_tmp_file:
+            image.resize([512, 512]).save(image_tmp_file)
+            text_out = client.predict(image_tmp_file.name, text, api_name="/single_chat")
+        return text_out
+    return chat
+
+
+class ChatTiOClient():
+    def __init__(self, url="http://127.0.0.1:7860/"):
+        self.model = get_model(url)
+
+    def reset(self):
+        pass
+
+    def chat(self, image: PILImage.Image, text: str, dialog = [], *args):
+        def get_bboxes(message):
+            if "region:" in message:
+                bboxes = sbbox_to_bbox(message) * np.asarray([*image.size, *image.size])
+                bboxes = [b.astype(int).reshape(4).tolist() for b in bboxes]
+            else:
+                bboxes = []
+            return bboxes
+
+        # 获取对话结果
+        text_dialog = "".join([f" human: {i[0]}\n agent: {i[1]}\n" for i in dialog])
+        text_dialog += f" human: {text}\n"
+        text_response = self.model(image, text_dialog.lower())
+        bboxes_1 = get_bboxes(text_response)
+
+        # 获取grounding结果
+        text_dialog += f" agent: {text_response}\n"
+        text_grounding_query = f" \n#instruction: which region does the context describe?\n#context: \"{text_dialog}\""
+        text_grounding = self.model(image, text_grounding_query.lower())
+        bboxes_2 = get_bboxes(text_grounding)
+
+        dialog += [(text, text_response)]
+        return {
+            "text_response": text_response,
+            "dialog": dialog,
+            "bboxes": bboxes_1 + bboxes_2
+        }
